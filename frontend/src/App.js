9a66cfd231864fedb7a2a89f0ee3fe30b56dc113
Sanjay-RD <rishidevsanjay9@gmail.com>
2021-02-16 12:35:21 +0545

    Header & Footer Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./App.css";
 
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import { Container } from "react-bootstrap";
+
 function App() {
   return (
     <>
-      <h1>Welcome to the proshop</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to the proshop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
