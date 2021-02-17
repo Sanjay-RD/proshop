5eff37c9c82497127c81bfe10f08f79664178be1
Sanjay-RD <rishidevsanjay9@gmail.com>
2021-02-17 17:33:29 +0545

    ES Modules In Node.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const app = express();
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
 
-const products = require("./data/products");
+const app = express();
 
 dotenv.config();
 
