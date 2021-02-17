1ddc356effbe15258e4306e903acde1ad5f12a82
Sanjay-RD <rishidevsanjay9@gmail.com>
2021-02-17 17:27:14 +0545

    Environment Variables

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const app = express();
 
 const products = require("./data/products");
+
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("hello");
@@ -16,4 +19,9 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log(`Server Running on port 5000`));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server Running in ${process.env.NODE_ENV} mode on port 5000`)
+);
